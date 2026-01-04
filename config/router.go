fbfe733e5d8da66ab86d80aa2fc09c636732245d
fikrialwan <fikriar93@gmail.com>
2026-01-04 17:33:16 +0700

    feat: allowed cors for some url define by env

--- config/router.go
+++ config/router.go
@@ -3,15 +3,35 @@ package config
 import (
 	"kambing-cup-backend/router"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 func SetupRouter(pool *pgxpool.Pool) *chi.Mux {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
+	// Configure CORS
+	allowedOriginsStr := os.Getenv("ALLOWED_ORIGINS")
+	var allowedOrigins []string
+	if allowedOriginsStr != "" {
+		allowedOrigins = strings.Split(allowedOriginsStr, ";")
+	}
+
+	corsMiddleware := cors.New(cors.Options{
+		AllowedOrigins:   allowedOrigins,
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+	r.Use(corsMiddleware.Handler)
 
 	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
 		w.Write([]byte("welcome"))
